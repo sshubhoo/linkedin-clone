f34906b5f6323a5f2bcdff55af8172e039aea2ba
Shubhodeep Banerjee <sshubhodeepp@gmail.com>
2022-01-28 21:38:29 +0530

    Commit 4  - Left, Right, Header & Share Feed UI Dn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route }  from 'react-router-dom';
-import Login from './components/Login';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path='/'>
+          <Route exact path="/">
             <Login />
+          </Route>
+          <Route path="/home">
+            <Header />
+            <Home />
           </Route>
         </Switch>
       </Router>
---
+++ src/components/Main.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const Main = (props) => {
+  return <Container><Sharebox>
+            Share
+      </Sharebox>
+      </Container>;
+};
+
+const Container = styled.div`
+  grid-area: main;
+`;
+
+const CommonCard = styled.div`
+    text-align: center;
+    overflow: hidden;
+    margin-bottom: 8px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: relative;
+    border: none;
+    box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
+
+const Sharebox = styled(CommonCard)`
+    display: flex;
+    flex-direction: column;
+    color: #958b7b;
+    margin: 0 0 8px;
+    background: white;
+`;
+
+export default Main;
